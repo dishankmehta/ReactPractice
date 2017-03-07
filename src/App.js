1f68a4bd7cd9375fb446812d6e58a3e856b9bbee
Dishank Mehta <mdishank@gmail.com>
2017-03-07 17:33:37 +0530

    Few Changes

--- src/App.js
+++ src/App.js
@@ -15,4 +15,13 @@ class App extends React.Component{
     }
 }
 
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+};
+
+App.defaultProps = {
+  txt: "This is default txt"
+};
+
 export default App
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <App txt="This is a prop"/>,
+  <App cat={50}/>,
   document.getElementById('root')
 );
